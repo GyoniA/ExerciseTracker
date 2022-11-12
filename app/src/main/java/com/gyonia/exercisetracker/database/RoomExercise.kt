62cf146e72f1665987bc57d85c546bc2ae2e5676
GyoniA <gyoniakos@gmail.com>
2022-11-12 13:50:20 +0100

    Start of Room implementation

---
+++ app/src/main/java/com/gyonia/exercisetracker/database/RoomExercise.kt
@@ -0,0 +1,58 @@
+package com.gyonia.exercisetracker.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.gyonia.exercisetracker.model.Exercise
+
+object MapTypeConverter {
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToMap(value: String): HashMap<String, Int> {
+        return Gson().fromJson(value,  object : TypeToken<HashMap<String, Int>>() {}.type)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun mapToString(value: HashMap<String, Int>?): String {
+        return if(value == null) "" else Gson().toJson(value)
+    }
+}
+
+object ExerciseTypeConverter {
+    const val Reps = "Reps"
+    const val Time = "Time"
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToExerciseType(value: String): Exercise.ExerciseType {
+        return when (value) {
+            Reps -> Exercise.ExerciseType.Reps
+            Time -> Exercise.ExerciseType.Time
+            else -> Exercise.ExerciseType.Reps
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun exerciseTypeToString(value: Exercise.ExerciseType): String {
+        return value.name
+    }
+}
+
+@Entity(tableName = "exercise")
+data class RoomExercise(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val description: String,
+    @TypeConverters(ExerciseTypeConverter::class)
+    val type: Exercise.ExerciseType,
+
+    @TypeConverters(MapTypeConverter::class)
+    val amountDoneOnDate: HashMap<String, Int>
+)
